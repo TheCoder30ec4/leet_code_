f03cfc832590afd652b0f464f81c9cec248ea91a
CH Varun <130673937+TheCoder30ec4@users.noreply.github.com>
2024-08-06 12:07:06 +0530

    Time: 3 ms (10.25%), Space: 42.5 MB (6.5%) - LeetHub

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minimumPushes(String word) {
+
+        
+        int [] map = new int[128];
+        int toaltSum = 0;
+
+        for(int i=0;i<word.length();i++){
+
+            map[word.charAt(i)]++;
+        }
+
+        Arrays.sort(map);
+        for(int i=0;i<map.length/2;i++){
+            int temp = map[i];
+            map[i] = map[map.length-i-1];
+            map[map.length-i-1] = temp;
+
+        }
+
+        for(int i=0;i<map.length;i++){
+
+            toaltSum += (map[i] * ((i/8)+1));
+        }
+
+        return toaltSum;
+
+
+
+
+
+        
+
+
+        
+    }
+}
