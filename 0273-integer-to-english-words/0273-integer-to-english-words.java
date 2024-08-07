0fc4fe5725bc2882e48d0d6aec60ea145f1cc463
CH Varun <130673937+TheCoder30ec4@users.noreply.github.com>
2024-08-07 09:07:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String numberToWords(int num) {
+
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        map.put(1,"One");
+        map.put(2,"Two");
+        map.put(3,"Three");
+        map.put(4,"Four");
+        map.put(5,"Five");
+        map.put(6,"Six");
+        map.put(7,"Seven");
+        map.put(8,"Eight");
+        map.put(9,"Nine");
+
+        map.put(10,"Ten");
+        map.put(11,"Eleven");
+        map.put(12,"Twelve");
+        map.put(13,"Thirteen");
+        map.put(14,"Fourteen");
+        map.put(15,"Fifteen");
+        map.put(16,"Sixteen");
+        map.put(17,"Seventeen");
+        map.put(18,"Eighteen");
+        map.put(19,"Nineteen");
+
+        map.put(20,"Twenty");
+        map.put(20,"Twenty");
+        map.put(20,"Twenty");
+        map.put(20,"Twenty");
+        map.put(20,"Twenty");
+        
+
+
+
+
+        
+    }
+}
